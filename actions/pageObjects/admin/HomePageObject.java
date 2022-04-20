602ee36f0ece56dbfd27f749e342293fbdf284ae
Hoang Thi Tinh <hoangtinh1010@gmail.com>
2022-04-20 15:31:29 +0700

    push nốt phần structure của framework

---
+++ actions/pageObjects/admin/HomePageObject.java
@@ -0,0 +1,11 @@
+package pageObjects.admin;
+
+public class HomePageObject {
+    // Hàm để tương tác với các element
+public void clickToRegisterButton() {	
+}
+
+public void enterToEmailTextbox() {	
+}
+
+}
---
+++ interfaces/pageUIs/admin/HomePageUI.java
@@ -0,0 +1,6 @@
+package pageUIs.admin;
+
+public class HomePageUI {
+    //Define các locator (UI) của element thuộc page đó
+	public static final String REGISTER_BUTTON = "//button[@id='register']";
+}
